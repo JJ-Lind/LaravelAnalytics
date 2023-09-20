a8eda64a69b2afabf276e1b5bde9371a6628e4ec
JJ.Lind <jordan.j.lind1997@gmail.com>
2023-09-20 11:15:14 +0200

    Initial Commit

---
+++ src/LaravelAnalyticsServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace WezanEnterprises\LaravelAnalytics;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelAnalyticsServiceProvider extends ServiceProvider {
+
+    public function boot() { }
+
+    public function register() { }
+}
