a512be71e2690532fe580fde50a58f61f3fe08ef
JLind <J.lind@gmu.online>
2023-10-16 10:37:48 +0200

    Fixes

--- src/Client.php
+++ src/Client.php
@@ -47,7 +47,7 @@ class Client {
     {
         return match ($key) {
             'date' => Carbon::createFromFormat('Ymd', $value),
-            'visitors', 'pageViews', 'activeUsers', 'newUsers', 'screenPageViews', 'active1DayUsers', 'active7DayUsers', 'active28DayUsers' => (int) $value,
+            'visitors', 'pageViews', 'activeUsers', 'newUsers', 'screenPageViews', 'active1DayUsers', 'active7DayUsers', 'active28DayUsers', 'totalUsers' => (int) $value,
             default => $value
         };
     }
